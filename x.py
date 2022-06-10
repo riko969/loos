43df814eaab84fe3a5854487b70c6be0e03e3c02
riko969 <107186549+riko969@users.noreply.github.com>
2022-06-11 00:59:25 +0700

    Add files via upload

---
+++ x.py
@@ -0,0 +1,27 @@
+import sys
+import this
+import os
+
+
+def main():
+	os.system('clear')
+	os.system('figlet rikoputra')
+	banner = '''
+
+	[+]AUTHOR : rikoslengean
+	[+]WHATSAPP : +62 859-3112-6533
+	'''
+	print(banner)
+	no = input('nomor kamu : ')
+	target = input('target : ')
+
+	headers = {
+	'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.106 Safari/537.36'
+	'url = http://www.baidu.com'
+	'Host: x.hacking8.com'
+	}
+
+	for i in range(50):
+		threadpoo = ('http://www.baidu.com')
+
+main()
